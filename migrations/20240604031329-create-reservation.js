a7cef6b4d5e374ed9fdf04bb85364e7d4be0fd2e
Jose Lisboa <jmlisboa@uc.cl>
2024-07-07 16:48:53 -0400

    add migration to add column to reservation

--- migrations/20240604031329-create-reservation.js
+++ migrations/20240604031329-create-reservation.js
@@ -14,11 +14,6 @@ module.exports = {
         allowNull: false,
         defaultValue: false
       },
-      isReviewed: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-      },
       courseId: {
         type: Sequelize.INTEGER,
         references: {
