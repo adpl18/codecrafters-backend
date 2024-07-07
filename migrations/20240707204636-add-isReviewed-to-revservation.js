b313cc209447fe756937f829261e2dcdaecebd74
Jose Lisboa <jmlisboa@uc.cl>
2024-07-07 18:08:10 -0400

    fix isReviewed migration

--- migrations/20240707204636-add-isReviewed-to-revservation.js
+++ migrations/20240707204636-add-isReviewed-to-revservation.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Reservation', 'isReviewed', {
+    await queryInterface.addColumn('Reservations', 'isReviewed', {
       type: Sequelize.BOOLEAN,
       allowNull: false,
       defaultValue: false
