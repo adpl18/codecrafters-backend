3bf69ba99fd1a8a1cf732907a6036811f7208c21
sirodriguez1 <69883918+sirodriguez1@users.noreply.github.com>
2024-07-07 22:00:50 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: { ...globals.node, process: 'readonly' } }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        process: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        expect: 'readonly',
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
 
